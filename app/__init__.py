c49b040091ba6701b4c0922cb4bcaef2ebd0a72d
Nazarii Tsiuniak <nazarii.tsiuniak.23@pnu.edu.ua>
2025-10-20 20:12:21 +0300

    Tested users Blueprint routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-from app import views  
+    from app.users.views import users_bp
+    app.register_blueprint(users_bp, url_prefix="/users")
+
+    @app.route('/')
+    def index():
+        return '<h1>Головна сторінка</h1><p><a href="/users/hi/John?age=30">Перейдіть на /users/hi/John</a></p>'
+
+    return app
