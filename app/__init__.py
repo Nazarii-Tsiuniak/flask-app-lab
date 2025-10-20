13afaf2e388195e4d222ca7344e81698cfb2a0c6
Nazarii Tsiuniak <nazarii.tsiuniak.23@pnu.edu.ua>
2025-10-20 20:21:04 +0300

    Add products Blueprint with sample routes

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,15 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    # Users Blueprint
     from app.users.views import users_bp
     app.register_blueprint(users_bp, url_prefix="/users")
 
+    # Products Blueprint
+    from app.products.views import products_bp
+    app.register_blueprint(products_bp, url_prefix="/products")
+
+    # Головна сторінка
     @app.route('/')
     def index():
         return '<h1>Головна сторінка</h1><p><a href="/users/hi/John?age=30">Перейдіть на /users/hi/John</a></p>'
