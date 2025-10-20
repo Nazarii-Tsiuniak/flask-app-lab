f4d2cf80c201288d5463fa64d938349227c89d53
Nazarii Tsiuniak <nazarii.tsiuniak.23@pnu.edu.ua>
2025-10-21 01:32:22 +0300

    Full lab4

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from app.users.routes import users_bp
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = 'supersecretkey'
 
-    # Users Blueprint
-    from app.users.views import users_bp
-    app.register_blueprint(users_bp, url_prefix="/users")
+    app.register_blueprint(users_bp, url_prefix='/users')
 
-    # Products Blueprint
-    from app.products.views import products_bp
-    app.register_blueprint(products_bp, url_prefix="/products")
-
-    # Головна сторінка
+    # Головна сторінка одразу перенаправляє на логін
     @app.route('/')
-    def index():
-        return '<h1>Головна сторінка</h1><p><a href="/users/hi/John?age=30">Перейдіть на /users/hi/John</a></p>'
+    def home():
+        return redirect(url_for('users.login'))
 
     return app
